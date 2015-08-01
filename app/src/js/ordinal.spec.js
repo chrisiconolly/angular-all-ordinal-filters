6a6e25123889f687d4d82823f9c58b2646da9bea
Chris Conolly <chris.conolly@wonga.com>
2015-08-01 22:30:50 +0100

    Tidying up and adding comments to spec file  - Also adds source for the "using" method

--- app/src/js/ordinal.spec.js
+++ app/src/js/ordinal.spec.js
@@ -1,4 +1,18 @@
 /* global describe, beforeEach, module, inject, it, expect, jasmine, console */
+
+/**
+ * Using - a data provider method for jasmine unit tests.
+ * Source: http://blog.jphpsf.com/2012/08/30/drying-up-your-javascript-jasmine-tests
+ *
+ * @param string name
+ *   Name to give to the "using" block. This will be used in the test
+ *   description
+ * @param mixed values
+ *   Arguments to pass through to the data provider iteration.
+ *   The iteration can take multiple arguments bu providing an array.
+ * @param function func
+ *   The data provider iteration, put your unit tests in this block.
+ */
 function using(name, values, func) {
   for (var i = 0, count = values.length; i < count; i++) {
     if (Object.prototype.toString.call(values[i]) !== '[object Array]') {
@@ -10,13 +24,13 @@ function using(name, values, func) {
   }
 }
 
+
 describe('Ordinal filters', function () {
 
   var $filter;
 
   beforeEach(function () {
     module('ordinal');
-
     inject(function (_$filter_) {
       $filter = _$filter_;
     });
